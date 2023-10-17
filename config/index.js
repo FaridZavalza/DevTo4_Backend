9acb19e2f36a8a3187d6d0928be3ed11fa93858a
Farid Zavalza <faridzavalza@outlook.com>
2023-10-16 21:50:47 -0600

    Carpetas creadas

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 require("dotenv").config()
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    DB_URI: "mongodb+srv://challengeDB-4:danielrmmata@cluster0.nqao7ru.mongodb.net/challenge-4"
 }
