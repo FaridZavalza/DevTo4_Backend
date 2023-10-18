afc9b7dd9c2ad5f695f8983cd271175212dc2a23
Noe Castro <noec5298@gmail.com>
2023-10-18 17:55:14 -0500

    se anadio model users1

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,5 @@
 const {Schema} = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const uniqueValidatior = require('mongoose-unique-validator')
 const userSchema = new Schema({
     name: {
